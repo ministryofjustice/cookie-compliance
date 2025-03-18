12ebb0955f7b5d1a114f9836a89a60fc49080109
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2025-03-18 14:37:54 +0000

    Add Settings Page

--- inc/settings.php
+++ inc/settings.php
@@ -1,8 +1,33 @@
 <?php
 
 
-function cookie_consent_scripts() {  
-    wp_register_style('cookie-consent-styles', plugins_url('../dist/css/style.min.css', __FILE__));
-    wp_enqueue_style("cookie-consent-styles");
+function cookie_compliance_scripts() {  
+    wp_register_style('cookie-compliance-styles', plugins_url('../dist/css/style.min.css', __FILE__));
+    wp_enqueue_style("cookie-compliance-styles");
 }
-add_action('wp_enqueue_scripts', 'cookie_consent_scripts');
+add_action('wp_enqueue_scripts', 'cookie_compliance_scripts');
+
+// Register query variable for endpoint
+function cookie_compliance_register_query_vars($vars) {
+    $vars[] = 'cookie_settings_page';
+    return $vars;
+}
+add_filter('query_vars', 'cookie_compliance_register_query_vars');
+
+// Add rewrite rule for endpoint
+function cookie_compliance_rewrite_rule() {
+    add_rewrite_rule('^cookies/?', 'index.php?cookie_settings_page=true', 'top');
+}
+add_action('init', 'cookie_compliance_rewrite_rule');
+
+// Handle the custom endpoint
+function cookie_compliance_template_redirect() {
+    global $wp_query;
+
+    if (isset($wp_query->query_vars['cookie_settings_page'])) {
+        // Load specific template file
+        include(plugin_dir_path(dirname(__FILE__)) . 'templates/cookie-settings-page.php');
+        exit;
+    }
+}
+add_action('template_redirect', 'cookie_compliance_template_redirect');
---
+++ templates/cookie-settings-page.php
@@ -0,0 +1,100 @@
+<?php
+get_header();
+
+flush();
+    ?>
+
+<div id="primary">
+    <h1>Cookie settings</h1>
+    <p>Here you can manage your cookie settings for this website.</p>
+
+    <h2>Marketing cookies</h2>
+
+    <h3>Google Analytics 4</h3>
+    <p>We use Google Analytics to measure how you use the website so we can improve it based on user needs. We do not allow Google Analytics to use or share the data about how you use this site.</p>
+
+    <table>
+        <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Purpose</th>
+            <th scope="col">Expires</th>
+        </tr>
+        </thead>
+        <tbody>
+                                                <tr>
+                <th scope="row">_ga</th>
+                <td>Used to distinguish users.</td>
+                <td>2 years</td>
+            </tr>
+                                                    <tr>
+                <th scope="row">_ga_JYSFWSDPV7</th>
+                <td>Used to persist session state.</td>
+                <td >2 years</td>
+            </tr>
+            
+        </tbody>
+    </table>
+    
+    <h2>Third-party cookies</h2>
+
+    <h3>Video Streaming</h3>
+    <p>We have no control over cookies set by third parties. You can turn them off, but not through us.</p>
+
+    <h3>Social Media</h3>
+    <p>If you share a link to a page, the service you share it on (for example, Facebook) may set a cookie.</p>
+
+    <h2>Essential cookies</h2>
+
+    <h3>All users</h3>
+    <p>These cookies will always need to be on because they make our site work.</p>
+
+    <table>
+        <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Purpose</th>
+            <th scope="col">Expires</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row" >wordpress_test_cookie</th>
+                <td>This is used to test whether the browser accepts cookies</td>
+                <td >When you close your browser</td>
+            </tr>
+                                                    <tr>
+                <th scope="row" class="ccfw-banner__table-header">PHPSESSID</th>
+                <td>This is used to link your device to the information sent to the server from your browser. It is typically used to avoid you having to retype information when moving from one page to another.</td>
+                <td>When you close your browser</td>
+            </tr>
+                                                    <tr>
+                <th scope="row" class="ccfw-banner__table-header">ccfw-banner-hidden</th>
+                <td>Lets us know you have chosen which cookies are used so we can stop the cookie banner appearing when you return to the site.</td>
+                <td>1 year</td>
+            </tr>
+                                                    <tr>
+                <th scope="row" class="ccfw-banner__table-header">ccfw-gtm-allowed</th>
+                <td>Lets us know you have accepted certain cookies so we can stop them loading when you return to the site.</td>
+                <td>1 year</td>
+            </tr>
+                                                    <tr>
+                <th scope="row" class="ccfw-banner__table-header">ccfw-time</th>
+                <td>This is used to store the cookie expiry dates so we know when to ask you again for consent.</td>
+                <td>1 year</td>
+            </tr>
+                                                    <tr>
+                <th scope="row" class="ccfw-banner__table-header">info_banner_dismissed</th>
+                <td>This remembers if an information banner has been dismissed and prevents it from being displayed again.</td>
+                <td>When you close your browser</td>
+            </tr>
+            
+        </tbody>
+    </table>
+
+    <h3>Logged in users</h3>
+    <p>These cookies will always need to be on because they make our site work.</p>
+</div>
+<?php
+flush();
+get_footer();
