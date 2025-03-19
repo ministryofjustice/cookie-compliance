c954add99b9499e6748952ca25d5de21d88ca5de
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-03-19 15:57:51 +0000

    Styled cookie banner

--- templates/cookie-banner.php
+++ templates/cookie-banner.php
@@ -1,28 +1,55 @@
-<div id="ccfw-page-banner" data-nosnippet="true">
-    <div class="ccfw-banner">
-        <div class="ccfw-banner__intro">
-            <h2 class="ccfw-banner__heading">
-                Are you OK with cookies?            </h2>
-            <p class="ccfw-banner__info-text">
-                We use small files called ‘cookies’ on ccrc.gov.uk.  Some are essential to make the site work, some help us to understand how we can improve your experience, and some are set by third parties. You can choose to turn off the non-essential cookies. Which cookies are you happy for us to use?            </p>
-            <div class="ccfw-banner__buttons">
-                <button class="ccfw-banner__button" id="cookie-accept" type="submit" aria-label="I am OK with cookies. Page will reload.">
-                    I am OK with cookies                </button>
-
-                <button class="ccfw-banner__button" id="cookie-decline" type="submit">
-                    Only use essential cookies                </button>
-
-                <button type="button" class="ccfw-banner-button ccfw-banner__button--expand-options" id="cookie-more-info">
-                    Choose which cookies we use                    <svg class="ccfw-banner__button--arrow" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.0516 1.86827C10.4659 1.28249 9.51612 1.28249 8.93033 1.86827C8.34454 2.45406
-                        8.34454 3.40381 8.93033 3.98959L11.0516 1.86827ZM17.0621 10L18.1227 11.0607C18.7085 10.4749
-                        18.7085 9.52513 18.1227 8.93934L17.0621 10ZM8.93033 16.0104C8.34454 16.5962 8.34454 17.5459
-                        8.93033 18.1317C9.51612 18.7175 10.4659 18.7175 11.0516 18.1317L8.93033 16.0104ZM8.93033
-                        3.98959L16.0014 11.0607L18.1227 8.93934L11.0516 1.86827L8.93033 3.98959ZM16.0014 8.93934L8.93033
-                        16.0104L11.0516 18.1317L18.1227 11.0607L16.0014 8.93934Z"></path>
-                    </svg>
-                </button>
-            </div>
+<?php 
+$site_name = !empty(get_bloginfo()) ? get_bloginfo() : "this site"; 
+$site_url = get_site_url();
+/* */
+$display_cookie_banner = 1;
+if ($display_cookie_banner) {
+?>
+<div id="cookie-compliance-banner" data-nosnippet="true" class="bg-gray-200 w-full py-[1rem]">
+    <div class="mx-auto max-w-[90%] w-[960px]">
+        <h2 class="text-2xl">
+            Cookies on <?php echo $site_name;?>
+        </h2>
+        <div class="text-lg">
+            <p class="">
+                We use some essential cookies to make this service work.
+            </p>
+            <p class="">
+                We’d also like to use analytics cookies so we can understand how you use the service and make improvements.
+            </p>
+        </div>
+        <div class="text-lg">
+            <button
+                id="cookie-accept" type="submit"
+                class="text-white bg-green-900 hover:bg-green-950
+                        font-medium px-5 py-2.5 me-2 mb-2
+                        focus:text-black focus:bg-yellow-400 focus:outline-none
+                "
+            >
+                Accept analytics cookies
+            </button>
+            <button
+                id="cookie-decline" type="submit"
+                class="text-white bg-green-900 hover:bg-green-950
+                        font-medium px-5 py-2.5 me-2 mb-2
+                        focus:text-black focus:bg-yellow-400 focus:outline-none
+                "
+            >
+                Reject analytics cookies
+            </button>
+            <a
+                href="<?php echo $site_url;?>/cookies" id="cookie-page-link"
+                class="
+                    text-sky-700 focus:bg-yellow-400 hover:text-sky-900 focus:text-black focus:bg-yellow
+                    underline focus:no-underline border-b-4 focus:border-black focus:outline-none
+                    px-3 py-2.5
+                "
+            >
+                View cookies
+            </a>
         </div>
     </div>
 </div>
+
+<?php //*/
+}
