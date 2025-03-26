be07098385381df15df54574267714f23f6a5f28
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2025-03-26 09:48:05 +0000

    Cookie banner and page only show when GTM code is added

--- inc/settings.php
+++ inc/settings.php
@@ -3,16 +3,18 @@
 
 function cookie_compliance_scripts() {  
 
-    wp_register_style('cookie-compliance-tailwind', plugins_url('../dist/tailwind.css', __FILE__));
-    wp_enqueue_style("cookie-compliance-tailwind");
 
-    wp_register_style('cookie-compliance-styles', plugins_url('../dist/styles.css', __FILE__));
-    wp_enqueue_style("cookie-compliance-styles");
 
-    wp_register_script('cookie-consent-script', plugins_url('../dist/cookie-script.js', __FILE__));
+        add_action('wp_body_open', 'cookie_compliance_render_banner', 11);
+        
+        wp_register_style('cookie-compliance-tailwind', plugins_url('../dist/tailwind.css', __FILE__));
+        wp_enqueue_style("cookie-compliance-tailwind");
 
-    $options = get_option('cookie_compliance_settings');
-    if (!empty($options) && array_key_exists('gtm_code', $options) && !empty($options['gtm_code']) ) {
+        wp_register_style('cookie-compliance-styles', plugins_url('../dist/styles.css', __FILE__));
+        wp_enqueue_style("cookie-compliance-styles");
+
+        wp_register_script('cookie-consent-script', plugins_url('../dist/cookie-script.js', __FILE__));
+
         wp_localize_script(
             'cookie-consent-script',
             'cookie_consent_object',
@@ -20,10 +22,10 @@ function cookie_compliance_scripts() {
                 'gtmcode' => $options['gtm_code'],
             )
         );
-    }
-    wp_enqueue_script('cookie-consent-script');
+        wp_enqueue_script('cookie-consent-script');
+    
+   
 }
-add_action('wp_enqueue_scripts', 'cookie_compliance_scripts');
 
 // Register query variable for endpoint
 function cookie_compliance_register_query_vars($vars) {
@@ -34,7 +36,15 @@ add_filter('query_vars', 'cookie_compliance_register_query_vars');
 
 // Add rewrite rule for endpoint
 function cookie_compliance_rewrite_rule() {
-    add_rewrite_rule('^cookies/?', 'index.php?cookie_settings_page=true', 'top');
+    $options = get_option('cookie_compliance_settings');
+    if (!empty($options) && array_key_exists('gtm_code', $options) && !empty($options['gtm_code']) ) {
+
+        add_rewrite_rule('^cookies/?', 'index.php?cookie_settings_page=true', 'top');
+        add_action('template_redirect', 'cookie_compliance_template_redirect');
+        add_action('wp_body_open', 'cookie_compliance_render_banner', 11);
+        add_action('wp_enqueue_scripts', 'cookie_compliance_scripts');
+
+    }
 }
 add_action('init', 'cookie_compliance_rewrite_rule');
 
@@ -48,10 +58,7 @@ function cookie_compliance_template_redirect() {
         exit;
     }
 }
-add_action('template_redirect', 'cookie_compliance_template_redirect');
-
 
 function cookie_compliance_render_banner() {
     include(plugin_dir_path(dirname(__FILE__)) . 'templates/cookie-banner.php');
 }
-add_action('wp_body_open', 'cookie_compliance_render_banner', 11);
